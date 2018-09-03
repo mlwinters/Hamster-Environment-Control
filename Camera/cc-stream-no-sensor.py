4e144e2e891d12dd5ade2723d6ba96d380be962d
Morgan <7806037+mlwinters@users.noreply.github.com>
2018-09-03 02:39:21 +0100

    Moved Email Address

--- Camera/cc-stream-no-sensor.py
+++ Camera/cc-stream-no-sensor.py
@@ -1,9 +1,9 @@
 ### COPYRIGHT ###
 # Cage Camera Stream-no-sensor
-# Version: 0.1.6 Alpha
-# Copyright (C) 2018 Morgan Winters <morgan.l.winters@gmail.com
-# Author: Morgan Winters
-# Contributions: Dave Jones <dave@waveform.org.uk>
+# Version: 0.1.7 Alpha
+# Copyright (C) 2018 Morgan Winters
+# Author: Morgan Winters  <morgan.l.winters@gmail.com>
+# Contributions: Dave Jones  <dave@waveform.org.uk>
 # Created: 18/03/2018
 # Modified By: <name>, <date>
 #
@@ -70,7 +70,7 @@ except:
 ## Variables ##
 ## Static Variables ##
 # Version #
-Version = "0.1.6 Alpha"
+Version = "0.1.7 Alpha"
 #
 
 # Pet Name #
