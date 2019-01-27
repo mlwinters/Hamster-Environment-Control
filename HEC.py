023a9d5f1b3059c65aba5af14e611581dd918e0c
Morgan <7806037+mlwinters@users.noreply.github.com>
2019-01-27 17:50:10 +0000

    Update HEC.py

--- HEC.py
+++ HEC.py
@@ -111,7 +111,7 @@ GPIO.output(27, False)
 
 ## Sensor Setup ##
 try:
-  # This section was written by Adafruit Indusdstries and was taken from the #
+  # This section was written by Adafruit Industries and was taken from the #
   # example file included with the Adafruit BME280 library: #
   # https://github.com/adafruit/Adafruit_Python_BME280/blob/master/Adafruit_BME280_Example.py #
   sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
